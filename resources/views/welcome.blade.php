04b5dce9a5e9a6afe0e244c4cbaacff515f7bf7e
k_kO2ke <desunokora527@gmail.com>
2019-12-16 03:09:14 +0900

    title fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,7 @@
 @extends("layouts.main")
+@section("page-name")
+	""
+@endsection
 @section("body-contents")
 	<div class="w-100 position-relative">
 		<div style="font-size: 15px;" class="position-absolute w-100 myContainer thread-name top m-0">Welcome to Bluemine!!</div>
