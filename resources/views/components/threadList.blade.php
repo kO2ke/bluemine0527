7076c387c60f30cd9d3c2eb8f583313ed1d55d2e
近藤康介 <kosuke@k-kosukeMacBook.local>
2019-12-29 18:45:45 +0900

    thread owner layout change

--- resources/views/components/threadList.blade.php
+++ resources/views/components/threadList.blade.php
@@ -1,23 +1,21 @@
 <div class="myContainer thread-list top">
-		@if (count($threadArray) != 0)
-			<h4 class="myGreen label children pl-4 py-2">{{$listTitle}}</h4>
-			<div class="container-fluid">
-				<div class='list-item p-2 row'>
-				@foreach ($threadArray as $item)
-					
-						<div class="col-sm-8">
-							<a class='thread-title' href="/thread/id={{$item->id}}">{{$item->title}}</a>	
-						</div>
-						<div class="col-sm-4 text-right">
-							<span class="text-secondary">Recent Update:{{$item->updated_at}}</span>
-						</div>
-					
-				@endforeach
-				</div>
+	@if (count($threadArray) != 0)
+	<h4 class="myGreen label children pl-4 py-2">{{$listTitle}}</h4>
+	<div class="container-fluid">
+		@foreach ($threadArray as $item)
+		<div class='list-item p-2 row'>
+			<div class="col-sm-8">
+				<a class='thread-title' href="/thread/id={{$item->id}}">{{$item->title}}</a>
 			</div>
-		@endif
+			<div class="col-sm-4 text-right">
+				<span class="text-secondary">Recent Update:{{$item->updated_at}}</span>
+			</div>
+		</div>
+		@endforeach
+	</div>
+	@endif
 
-		@if ($isPutCreateBtn ?? false)
-			@include("components.addThreadForm",["thread_id" => $thread->id ?? 0])
-		@endif	
+	@if ($isPutCreateBtn ?? false)
+	@include("components.addThreadForm",["thread_id" => $thread->id ?? 0])
+	@endif
 </div>
--- resources/views/thread.blade.php
+++ resources/views/thread.blade.php
@@ -3,103 +3,119 @@
 @section("body-class","not-top")
 
 @section("body-contents")
-	<div class="w-100 myLightgreen text-light" style="height:50px;">
-		@if ($thread->parent == null)
-		<a class="label parents" href="/">>TOP</a>
-		@else
-		<a class="label parents" href="/thread/id={{$thread->parent->id}}">>{{$thread->parent->title}}</a>
-		@endif
-	</div>
+<div class="w-100 myLightgreen text-light" style="height:50px;">
+	@if ($thread->parent == null)
+	<a class="label parents" href="/">>TOP</a>
+	@else
+	<a class="label parents" href="/thread/id={{$thread->parent->id}}">>{{$thread->parent->title}}</a>
+	@endif
+</div>
 
-	<div class="shadow-lg myGreen myContainer p-2 m-4">
-		<div class="myContainer thread-name pl-3">
-			<h4>{{$thread->title}}</h4>
-		</div>
-		<div class="bg-light mt-1 py-3 rounded">
-		    <div class="container-fluid ">
-				<div class="row text-secondary">
+<div class="shadow-lg myGreen myContainer p-2 m-4">
+	<div class="myContainer thread-name pl-3">
+		<h4>{{$thread->title}}</h4>
+	</div>
+	<div class="bg-light mt-1 py-3 rounded">
+		<div class="container-fluid ">
+			<div class="row text-secondary">
+				<div class="col-4 row">
 					<div class="col-3 text-center">
-						<p>{{$thread->owner->name}}</p>
-						<img class="w-100"　alt="" src="http://placehold.jp/100x100.png?text=Owner Icon">
+						<img alt="" src="http://placehold.jp/50x50.png?text=Owner Icon">
 					</div>
-					<div class="col-9">
-						<div class="mt-3">{{$thread->description}}</div>
+					<div class="col-9 text-left">
+						<p class="pt-1">{{$thread->owner->name}}</p>
 					</div>
+				</div>
+				<div class="col-12">
+					<div class="mt-3">{{$thread->description}}</div>
+				</div>
+				<div class="col-6 offset-6">
+					<p class="test-secondary text-right pt-3">Updated At:{{$thread->updated_at}}</p>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 
-	@include("components.threadList",["listTitle" => "Children", "thread" => $thread, "threadArray" => $thread->children()->latest()->get() ?? [], "isPutCreateBtn" => true])
+@include("components.threadList",["listTitle" => "Children", "thread" => $thread, "threadArray" =>
+$thread->children()->latest()->get() ?? [], "isPutCreateBtn" => true])
 
-	<h3 class="border rounded text-center p-1 mt-3">POSTS</h3>
+<div class="container-fluid">
+	<div class="row p-3 mx-2 my-2 ">
+		@guest
+		Please <a class="" href="{{ route('register') }}">{{ __('Register') }} </a> or <a class=""
+			href="{{ route('login') }}">{{ __('Login') }}</a> To Post
+		@else
+		<div class="text-center col-3">
 
-	<div class="container-fluid">
-		<div class="row p-3 mx-2 my-2 ">
-			@guest
-				Please <a class="" href="{{ route('register') }}">{{ __('Register') }} </a> or <a class="" href="{{ route('login') }}">{{ __('Login') }}</a> To Post
-			@else
-				<div class="text-center col-3">
-					
-					<p>{{Auth::user()->name}}</p>
-					<img alt="" src="http://placehold.jp/150x150.png?text=Your Icon">
-				</div>
-				<div class="col-9 form">
-					<p>　</p>
-					<form action="/createPost" method="POST">
-						{{ csrf_field() }}
-						<input type="hidden" name="thread_id" value={{$thread->id}}>
-		
-						<div class="form-group">
-							<textarea class="form-control" id="content" name="content" rows="6" placeholder="Enter Message..."></textarea>
-						</div>
-						<div class="form-group">
-							<input type="submit" name="createPost" value="Post Message" class="btn btn-primary float-right">
-						</div>
-					</form>
-				</div>
-			@endguest
+			<p>{{Auth::user()->name}}</p>
+			<img alt="" src="http://placehold.jp/150x150.png?text=Your Icon">
 		</div>
-		@foreach ($thread->posts()->latest()->get() as $post)
-		{{-- make　Posting Css --}}
+		<div class="col-9 form">
+			<p>　</p>
+			<form action="/createPost" method="POST">
+				{{ csrf_field() }}
+				<input type="hidden" name="thread_id" value={{$thread->id}}>
+
+				<div class="form-group">
+					<textarea class="form-control" id="content" name="content" rows="6"
+						placeholder="Enter Message..."></textarea>
+				</div>
+				<div class="form-group">
+					<input type="submit" name="createPost" value="Post Message" class="btn btn-primary float-right">
+				</div>
+			</form>
+		</div>
+		@endguest
+	</div>
+
+	<div class="row">
+		<div class="col-9">
+
+			@foreach ($thread->posts()->latest()->get() as $post)
+			{{-- make　Posting Css --}}
 			@guest
-				{{$formatByUser = ""}}
+			{{$formatByUser = ""}}
 			@elseif( Auth::user()->id == $post->user_id )
-				{{$formatByUser = "ml-5 myLightgreen"}}
+			{{$formatByUser = "ml-5 myLightgreen"}}
 			@else
-				{{$formatByUser = "mr-5"}}
-		{{-- make　Posting Css --}}
+			{{$formatByUser = "mr-5"}}
+			{{-- make　Posting Css --}}
 			@endguest
-		<div class="row p-3 mx-1 my-5 {{ $formatByUser }} border rounded">
-			<div class="text-center col-12 d-md-none">
-				<div class="row" style="">
-					<div class="col-2 text-center">
-						<img style="width:25px;" alt="" src="http://placehold.jp/100x100.png?text=User Icon">
-					</div>
-					<div class="col-4 text-left" style="font-size:11px;">
-						<p  class="align-middle">{{$post->owner->name}}</p>
-					</div>
-					<div class="col-6 align-middle" style="font-size:11px;">
-						<div class="text-right w-100 text-secondary">{{$post->created_at}}</div>
-					</div>
-				</div>
-			</div>
-			<div class="mt-1 col-12">
-				<div>{{$post->content}}</div>
-			</div>
-				<div class="col-6 mt-3 offset-6 d-none d-md-flex">
+
+			<div class="row p-3 mx-1 my-3 {{ $formatByUser }} border rounded">
+				<div class="text-center col-12 d-md-none">
+					<div class="row" style="">
 						<div class="col-2 text-center">
-							<img style="width:35px;" alt="" src="http://placehold.jp/100x100.png?text=User Icon">
+							<img style="width:25px;" alt="" src="http://placehold.jp/100x100.png?text=User Icon">
 						</div>
-						<div class="col-4 text-left" style="font-size:10px;">
-							<p  class="align-middle">{{$post->owner->name}}</p>
+						<div class="col-4 text-left" style="font-size:11px;">
+							<p class="align-middle">{{$post->owner->name}}</p>
 						</div>
-						<div class="col-6 align-middle text-right" style="font-size:10px;">
-							<div class="w-100 text-secondary">{{$post->created_at}}</div>
+						<div class="col-6 align-middle" style="font-size:11px;">
+							<div class="text-right w-100 text-secondary">{{$post->created_at}}</div>
 						</div>
 					</div>
 				</div>
+
+				<div class="mt-1 col-12">
+					<div>{{$post->content}}</div>
+				</div>
+
+				<div class="col-6 mt-3 offset-6 d-none d-md-flex">
+					<div class="col-2 text-center">
+						<img style="width:35px;" alt="" src="http://placehold.jp/100x100.png?text=User Icon">
+					</div>
+					<div class="col-4 text-left" style="font-size:10px;">
+						<p class="align-middle">{{$post->owner->name}}</p>
+					</div>
+					<div class="col-6 align-middle text-right" style="font-size:10px;">
+						<div class="w-100 text-secondary">{{$post->created_at}}</div>
+					</div>
+				</div>
+			</div>
+			@endforeach
 		</div>
-		@endforeach
 	</div>
+</div>
 @endsection
