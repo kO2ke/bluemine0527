595c14dfc74df23cf1467b0163677ff4378179fa
近藤康介 <kosuke@k-kosukeMacBook.local>
2019-12-29 18:56:30 +0900

    fix degrade of thread update time

--- resources/views/thread.blade.php
+++ resources/views/thread.blade.php
@@ -29,7 +29,7 @@
 				<div class="col-12">
 					<div class="mt-3">{{$thread->description}}</div>
 				</div>
-				<div class="col-6 offset-6">
+				<div class="col-12">
 					<p class="test-secondary text-right pt-3">Updated At:{{$thread->updated_at}}</p>
 				</div>
 			</div>
